b4aea06df5b2f8ed6c4124088f67b5c486106131
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2026-05-12 12:32:12 +0300

    Updated

--- Part5/app/dbconnect.py
+++ Part5/app/dbconnect.py
@@ -4,9 +4,9 @@ def connection():
     # Edited out actual values
     conn = mysql.connector.connect( host='dbhost',
                             port=3306,
-                            database='allergen_info',
-                            user='pythonapp',
-                            password='secretPass123!')
+                            database=db_name,
+                            user=db_user,
+                            password=db_pass)
     c = conn.cursor()
 
     return c, conn
